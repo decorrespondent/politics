45b516789971bb9eaf9a7b84feb74c0b61082a49
dtokmetzis <dimitri@decorrespondent.nl>
2019-05-23 17:35:34 +0200

    first commit

---
+++ src/config.py
@@ -0,0 +1,4 @@
+PATH_TEMP_DATA = '~/Documents/projecten/politiek/data/temp/'
+PATH_VIZ = '~/Documents/projecten/politiek/visuals/'
+PATH_OUTPUT_DATA = '~/Documents/projecten/politiek/data/'
+PATH_SOURCE_DATA = '~/Documents/projecten/politiek/data/source_data/'
